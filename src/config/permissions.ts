e7ebeaa1ad2231cd0731aec4f6b42a0dc72dbf44
Georgi Dimitrov <georgedimmitrov@gmail.com>
2023-08-15 21:03:44 +0300

    Add default roles

---
+++ src/config/permissions.ts
@@ -0,0 +1,24 @@
+export const ALL_PERMISSIONS = [
+  // users
+  "users:roles:write", // Allowed to add a role to a user (Admin)
+  "users:roles:delete", // Allowed to remove a role from a user (Admin)
+
+  // posts
+  "posts:write",
+  "posts:read",
+] as const;
+
+export const PERMISSIONS = ALL_PERMISSIONS.reduce((acc, permission) => {
+  acc[permission] = permission;
+  return acc;
+}, {} as Record<(typeof ALL_PERMISSIONS)[number], (typeof ALL_PERMISSIONS)[number]>);
+
+export const USER_ROLE_PERMISSIONS = [
+  PERMISSIONS["posts:write"],
+  PERMISSIONS["posts:read"],
+];
+
+export const SYSTEM_ROLES = {
+  SUPER_ADMIN: "SUPER_ADMIN",
+  APPLICATION_USER: "APPLICATION_USER",
+};
